898c8c6ac88a7683c1cb6581b6695743b59477e3
Wilson <wilsonhsieh1216@gmail.com>
2025-12-16 09:05:25 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import usersRouter from './api/users';
 import './db';
 import cors from 'cors';
 
@@ -24,6 +25,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
